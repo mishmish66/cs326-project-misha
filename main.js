d84d4ed0af05e794d92c60a72652bc0c81f5613c
Misha Lvovsky <m.lvovsky@protonmail.com>
2022-08-09 02:26:37 -0400

    added the coveted sticky add button

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import { tableManager } from "./src/tableManager";
+
 const labels = [
     'January',
     'February',
@@ -33,3 +35,21 @@ const myChart = new Chart(
     document.getElementById('main-chart'),
     config
 );
+
+window.addEventListener("scroll", () => {
+    let window_bottom = window.visualViewport.pageTop + window.visualViewport.height;
+    const button = document.getElementById("add-button");
+    const default_bottom_pos = window.visualViewport.height + button.offsetHeight * 1.25;
+
+    if (window_bottom > default_bottom_pos) {
+        button.classList.remove("add-button-unstuck");
+        button.classList.add("add-button-stuck");
+    } else {
+        button.classList.remove("add-button-stuck");
+        button.classList.add("add-button-unstuck");
+    }
+})
+
+document.getElementById("add-button").addEventListener("click", () => {
+
+})
