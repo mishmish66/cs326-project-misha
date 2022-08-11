e36b538e56b9960f6d0f948b600dc0c4348a01be
Misha Lvovsky <m.lvovsky@protonmail.com>
2022-08-11 00:52:48 -0400

    changed class to singleton

--- src/Motor_Data.js
+++ src/Motor_Data.js
@@ -1,28 +1,26 @@
-export class Motor_Data {
-    constructor() {
-        this.data = {
-            1: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-            2: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-            3: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-            4: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-            5: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-            6: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-            7: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-            8: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-            9: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-            10: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-            11: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-            12: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-            13: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-            14: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-            15: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-            16: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-            17: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-            18: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-        };
-    }
+export const Motor_Data = {
+    data: {
+        1: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
+        2: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
+        3: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
+        4: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
+        5: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
+        6: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
+        7: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
+        8: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
+        9: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
+        10: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
+        11: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
+        12: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
+        13: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
+        14: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
+        15: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
+        16: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
+        17: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
+        18: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
+    },
 
-    get_motors_array() {
+    get_motors_array: function () {
         const keys = Object.keys(this.data);
         const as_array = keys.map(key => {
             const copy = {};
@@ -31,11 +29,12 @@ export class Motor_Data {
             return copy;
         });
         return as_array;
-    }
+    },
 
-    key_sort(key, ascending) {
+    key_sort: function (key, ascending) {
         let arr = this.get_motors_array();
         arr.sort((lhs, rhs) => lhs[key] > rhs[key]);
         return arr;
-    }
+    },
 }
+
