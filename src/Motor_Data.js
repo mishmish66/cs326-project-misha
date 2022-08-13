cb176db7d56cc960881895bf2765c8a926ce0936
Misha Lvovsky <m.lvovsky@protonmail.com>
2022-08-12 23:23:37 -0400

    changed to single time use factory instead of object, and added getter and setter to make callback function

--- src/Motor_Data.js
+++ src/Motor_Data.js
@@ -1,40 +1,63 @@
-export const Motor_Data = {
-    data: {
-        1: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-        2: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-        3: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-        4: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-        5: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-        6: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-        7: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-        8: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-        9: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-        10: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-        11: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-        12: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-        13: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-        14: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-        15: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-        16: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-        17: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-        18: { name: "hellow", power: "world", torque: "your-mom", price: "onoin" },
-    },
+export const Motor_Data = (() => {
+    const subscribed = {};
+    var next_motor_id = 0;
+    const data = {};
 
-    get_motors_array: function () {
-        const keys = Object.keys(this.data);
-        const as_array = keys.map(key => {
-            const copy = {};
-            Object.assign(copy, this.data[key]);
-            copy.motor_id = key;
-            return copy;
+    function change() {
+        Object.keys(subscribed).forEach(key => {
+            const func = subscribed[key];
+            func();
         });
-        return as_array;
-    },
+    }
 
-    key_sort: function (key, ascending) {
-        let arr = this.get_motors_array();
-        arr.sort((lhs, rhs) => lhs[key] > rhs[key]);
-        return arr;
-    },
-}
+    return {
+        assign_field: function (motor_id, field, value) {
+            if (this.valid_id(motor_id)) {
+                data[motor_id][field] = value;
+                change();
+            } else { return false; }
+        },
+        valid_id: function (motor_id) {
+            return motor_id in data;
+        },
+        id_field: function (motor_id, field) {
+            const motor = data[motor_id];
+            if (field in motor) {
+                return data[motor_id][field];
+            } else { return false; }
+        },
+        get_motors_array: function () {
+            const keys = Object.keys(data);
+            const as_array = keys.map(key => {
+                const copy = {};
+                Object.assign(copy, data[key]);
+                copy.motor_id = key;
+                return copy;
+            });
+            return as_array;
+        },
+
+        key_sort: function (key, ascending) {
+            let arr = this.get_motors_array();
+            arr.sort((lhs, rhs) => lhs[key] > rhs[key]);
+            return arr;
+        },
+
+        add_motor: function (motor) {
+            data[next_motor_id] = motor;
+            change();
+            return next_motor_id++;
+        },
+
+        subscribe: function (name, func) {
+            subscribed[name] = func;
+        },
+        unsubscribe: function (name) {
+            if (name in subscribed) {
+                delete subscribed[name];
+                return true;
+            } else { return false; }
+        },
+    }
+})();
 
