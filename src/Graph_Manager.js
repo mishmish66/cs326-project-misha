a1f3607e7284de2de1e46074718a520ed5e5d4e2
Misha Lvovsky <m.lvovsky@protonmail.com>
2022-08-12 23:22:20 -0400

    changed it to dynamically display the data in Motor_Data

--- src/Graph_Manager.js
+++ src/Graph_Manager.js
@@ -1,33 +1,57 @@
-import { Motor_Data } from "./Motor_Data";
+import { Motor_Data } from "./Motor_Data.js";
+// const Chart = require("chart.js");
 
-const labels = [];
-
-const data = {
-    labels: labels,
-    datasets: [{
-        // label: 'My First dataset',
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: [0, 10, 5, 2, 20, 30, 45],
-    }]
-};
-
-const config = {
-    type: 'line',
-    data: data,
-    options: {
-        // layout: {
-        //     padding: 100
-        // },
-        // maintainAspectRatio: false,
-        responsive: true,
+export const Graph_Manager = (() => {
+    const graph_element = document.getElementById('main-chart');
+    const data = {
+        datasets: [{
+            labels: [],
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [],
+        }],
+        options: {
+        }
     }
-};
+    const config = {
+        type: 'scatter',
+        data: data,
+        options: {
+            responsive: true,
+            plugins: { // This is from here: https://stackoverflow.com/questions/44661671/chart-js-scatter-chart-displaying-label-specific-to-point-in-tooltip
+                tooltip: {
+                    callbacks: {
+                        label: function (ctx) {
+                            let label = ctx.dataset.labels[ctx.dataIndex];
+                            // label += " (" + ctx.parsed.x + ", " + ctx.parsed.y + ")";
+                            return label;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return {
+        render: function () {
+            const full_motor_data = Motor_Data.key_sort('name', true);
+            data.datasets[0].labels = full_motor_data.map(datum => datum.name);
+            data.datasets[0].data = full_motor_data.map(datum => {
+                return {
+                    label: datum.name,
+                    x: datum.price,
+                    y: datum.power,
+                }
+            });
+            this.chart = new Chart(
+                graph_element,
+                config,
+            )
+        },
+    }
+})();
 
-export const Graph_Manager = {
-    render: function () {
-        full_motor_data = Motor_Data.key_sort('name', true);
-        labels = full_motor_data.map(datum => datum.name);
-        data.datasets.data = full_motor_data.map(datum => datum.power);
-    },
-}
+Motor_Data.subscribe("graph-render", () => {
+    Graph_Manager.chart.destroy();
+    Graph_Manager.render();
+});
