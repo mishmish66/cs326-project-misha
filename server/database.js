c3cf08420bdc5d8d983bc1e71719492c679dbb05
Misha Lvovsky <m.lvovsky@protonmail.com>
2022-08-24 01:47:25 -0400

    added a few rest endpoints

--- server/database.js
+++ server/database.js
@@ -1,21 +1,68 @@
-import { readFile, writeFile } from 'fs/promises';
+import { readFile } from 'fs/promises';
 import pg from 'pg'
 
 const { Client } = pg
 
-export const database = async () => {
-    const config = json.parse(
+export const database = await (async() => {
+    const config = JSON.parse(
         await readFile("./server/pgconfig.json", 'utf8')
     );
     const client = new Client(config);
-    await this.client.connect();
+    await client.connect();
 
     const init_text = `
     create table if not exists motors (
-        id integer,
-        name varchar(30),
-        power real,
-        weight real,
-        torque real
-    )`
-}
+        id serial not null primary key,
+        name varchar(128),
+        power double precision,
+        torque double precision,
+        price double precision,
+        mass double precision
+    )`;
+    const res = await client.query(init_text);
+
+    return {
+        add_motor: async (motor) => {
+            const query = {
+                text: `
+                INSERT INTO motors
+                VALUES (DEFAULT, ${motor.name}, ${motor.power}, ${motor.torque}, ${motor.price}, ${motor.mass})
+                RETURNING *`,
+                rowmode: 'array'
+            };
+            const res = await client.query(query);
+            return res.rows[0];
+        },
+        delete_motor: async (id) => {
+            const query = { text: `DELETE FROM motors WHERE id = ${id}` }
+            await client.query(query)
+        },
+        get_motor_by_id: async (id) => {
+            const query = { text: `SELECT * FROM motors WHERE id = ${id}`, rowmode: 'array' };
+            const data = await client.query(query).rows;
+        },
+        get_motor_ids: async () => {
+            const query = { text: 'SELECT id FROM motors', rowmode: 'array' };
+            const data = (await client.query(query)).rows;
+        },
+        get_motors: async () => {
+            const query = { text: 'SELECT * FROM motors', rowmode: 'array' };
+            const data = (await client.query(query)).rows;
+            return data;
+        },
+        update_motor: async (motor) => {
+            const query = {
+                text: `
+                    UPDATE motors SET
+                    name = ${motor.name},
+                    power = ${motor.power},
+                    torque = ${motor.torque},
+                    price = ${motor.price},
+                    mass = ${motor.mass},
+                    WHERE id = ${motor.id}
+                    RETURNING *`
+            }
+        },
+        close: async () => { await client.release() },
+    }
+})()
--- server/index.js
+++ server/index.js
@@ -3,12 +3,36 @@ import logger from 'morgan';
 
 import { database } from './database.js';
 
+console.log(database);
+
 const app = express();
 const port = process.env.PORT || 3000;
-
-database.connect();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'));
 app.use('/', express.static('client'));
+
+const motor_values = ['name', 'power', 'torque', 'price', 'mass'];
+
+app.post('/addMotor', async (request, response) => {
+    const motor = request.body.motor;
+    if (motor_values.reduce((acc, val) => val in motor && acc, true)) {
+        response.status(200).json({
+            status: `success`,
+            motor: await database.add_motor(body.motor),
+        });
+    } else
+        response.status(400).json({ error: `Invalid add motor request. Needs name, power, torque, price, and mass in motor in body` });
+});
+
+app.get('/getMotors', async (request, response) => {
+    response.status(200).json({
+        status: 'success',
+        motors: await database.get_motors(),
+    });
+})
+
+app.listen(port, () => {
+        // This is totally just for fun!
+    }); 
