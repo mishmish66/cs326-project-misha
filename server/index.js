1abdf2a6701c0dd3121fd79d8e0137355257a013
Misha Lvovsky <m.lvovsky@protonmail.com>
2022-08-24 23:48:44 -0400

    cleaned up code and added updateMotor endpoint

--- server/index.js
+++ server/index.js
@@ -3,8 +3,6 @@ import logger from 'morgan';
 
 import { database } from './database.js';
 
-console.log(database);
-
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -16,8 +14,8 @@ app.use('/', express.static('client'));
 const motor_values = ['name', 'power', 'torque', 'price', 'mass'];
 
 app.post('/addMotor', async (request, response) => {
-    const motor = request.body.motor;
-    if (motor_values.reduce((acc, val) => val in motor && acc, true)) {
+    const body = request.body;
+    if ('motor' in body && motor_values.reduce((acc, val) => val in body.motor && acc, true)) {
         response.status(200).json({
             status: `success`,
             motor: await database.add_motor(body.motor),
@@ -33,6 +31,27 @@ app.get('/getMotors', async (request, response) => {
     });
 })
 
+app.post('/updateMotor', async (request, response) => {
+    const body = request.body;
+    if ('motor' in body && motor_values.reduce((acc, val) => val in body.motor && acc, true)) {
+        response.status(200).json({
+            status: `success`,
+            motor: await database.update_motor(body.motor),
+        });
+    } else
+        response.status(400).json({ error: `Invalid update motor request. Needs name, power, torque, price, and mass in motor in body` });
+})
+
+app.delete('/deleteMotor', async (request, response) => {
+    const id = request.body.id;
+    if (database.delete_motor(id)) {
+        response.status(200).json({
+            status: 'success',
+        })
+    } else
+        response.status(400).json({ error: 'id in delete motor request must exist' });
+})
+
 app.listen(port, () => {
-        // This is totally just for fun!
-    }); 
+    // This is totally just for fun!
+}); 
